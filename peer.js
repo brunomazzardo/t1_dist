b314a96246f911b86e4427c1a45dd2b4c1ceeb6c
Bruno Mazzardo <bruno.mazzardo52@gmail.com>
2019-04-16 20:27:24 -0300

    implementando request_file para outros peers

--- peer.js
+++ peer.js
@@ -56,6 +56,9 @@ function superPeer(config){
                     if (err) throw err;
                     console.log('UDP message sent to ' + remote.address +':'+ remote.port);
                 });
+                break;
+            case "request_file_mc":
+
 
         }
     });
@@ -72,27 +75,23 @@ function buildMessage(type,content){
 
 
 
-
-function peer(config){
-
-    var stdin = process.openStdin();
-
+function readDir(config){
     let files = []
-
     const dir = fs.readdirSync(config.directoryPath)
-
     dir.forEach(function (file) {
         const fileObject = fs.readFileSync(config.directoryPath + file)
         let fileData = { fileName: file, hash: checksum(fileObject) }
         files.push(fileData)
     });
+    return files
+}
 
 
 
-    const buffer  = buildMessage("request_connection",files)
-
+function peer(config){
+    const stdin = process.openStdin();
+    const buffer  = buildMessage("request_connection",readDir(config))
     const client = dgram.createSocket('udp4');
-
     client.on('message', function (message, remote) {
         let messageParsed = JSON.parse(message)
         switch (messageParsed.type) {
@@ -100,17 +99,11 @@ function peer(config){
                 console.log(messageParsed.content)
         }
     })
-
     client.bind(config.port, config.ip);
-
-
-
     client.send(buffer, 0, buffer.length, config.sp_port, config.ip, function(err, bytes) {
         if (err) throw err;
         console.log('UDP message sent to ' + config.ip +':'+ config.sp_port);
     });
-
-
     stdin.addListener("data", function(d) {
         const input = d.toString().trim().split(" ")
         const action  = input[0]
@@ -123,8 +116,6 @@ function peer(config){
                 });
         }
     });
-
-
 }
 
 
